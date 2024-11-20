d21005653719701cb4b786bcd207b8823081b4bc
Gabriel de Santana <jgabriel.san07@gmail.com>
2024-11-20 13:48:32 -0300

    update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-import express from "express"
+// Import the Express
+import express from "express";
+
+// Defining o express em uma const to use
+const app = express();
+// 3000 = port
+app.listen(3000, () => {
+    console.log("Server listening...");
+});
+
+// Make a route
+app.get("/api", (req, res) => {
+    // send a response from status 200
+    res.status(200).send("Welcome to immersion!");
+});
